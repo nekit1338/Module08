bcc213507121a1c3fdefb1f80fdf0ec2e268abd1
nekit1338 <47562425+nekit1338@users.noreply.github.com>
2024-09-01 21:55:11 +0500

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,13 @@
+def add_everything_up(a, b):
+    try:
+        return a + b
+    except TypeError:
+        return str(a) + str(b)
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
+print(add_everything_up(False, True))
+print(add_everything_up([], False))
+print(add_everything_up([], {}))
